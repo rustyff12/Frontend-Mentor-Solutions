6012d84f180fc171735a450c1234999fbf67946d
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-19 07:17:00 +0200

    color gradients added. Layout shifts

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,18 @@ function App() {
       challengeLink={item.challengeLink}
       url={item.url}
       image={item.image}
+      index={item.id}
     />
   ));
   return (
     <main className="min-h-screen bg-zinc-950 p-8 md:flex md:flex-col md:justify-center md:p-16 ">
       <header className="text-center mb-8">
-        <h1 className="text-gray-400 text-4xl">Frontend Mentor Challenges</h1>
+        <h1 className="text-gray-200 text-3xl md:text-4xl lg:text-6xl flex flex-col items-center font-bold gap-2 font-oswald">
+          <span className="underline">Frontend Mentor Challenge</span>
+          <span className="bg-linear-to-r from-blue-500 to-pink-700  bg-clip-text text-transparent w-32 md:w-36 lg:w-58">
+            Soultions
+          </span>
+        </h1>
       </header>
       <section className="flex flex-col gap-8 md:grid md:grid-cols-2 lg:grid-cols-3  xl:max-w-[1600px]">
         {cardElements}
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,6 +4,7 @@ type CardProps = {
   challengeLink: string;
   url: string;
   image?: string;
+  index: number;
 };
 
 export default function Card({
@@ -12,28 +13,47 @@ export default function Card({
   challengeLink,
   url,
   image,
+  index,
 }: CardProps) {
+  const lgGradients = [
+    "from-blue-500 to-indigo-500",
+    "from-violet-500 to-purple-500",
+    "from-fuchsia-500 to-pink-500",
+  ];
+  const mdGradients = [
+    "from-blue-500 to-violet-500",
+    "from-purple-500 to-pink-500",
+  ];
+  const lgGradient = lgGradients[index % lgGradients.length];
+  const mdGradient = mdGradients[index % 2];
+
   return (
-    <article className="border-4 border-gray-200 rounded-xl p-6 grid grid-cols-1 grid-rows-[80px_auto_auto_auto_auto] gap-2 text-gray-200">
-      <div className="row-start-1 row-end-2 flex items-start">
-        <h2 className="text-2xl uppercase">{name}</h2>
-      </div>
-      <div className="row-start-2 row-end-4">
-        <a
-          className="group block overflow-hidden rounded-lg"
-          href={url}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src={image || "desktop-design.jpg"}
-            alt={`An image of the ${name} website`}
-            className="transition-transform duration-300 group-hover:scale-110 "
-          />
-        </a>
-      </div>
-      <div className="row-start-4 row-end-6">
-        <a href={challengeLink}>{description}</a>
+    <article
+      className={`p-[4px] rounded-xl bg-gradient-to-r  from-blue-500 to-pink-500 md:${mdGradient} lg:${lgGradient}`}
+    >
+      <div
+        className={`rounded-xl bg-zinc-950 p-6 grid grid-cols-1 grid-rows-[80px_auto_auto_auto_auto] gap-2 text-gray-200 h-full `}
+      >
+        <div className="row-start-1 row-end-2 flex items-start">
+          <h2 className="text-3xl uppercase">{name}</h2>
+        </div>
+        <div className="row-start-2 row-end-4">
+          <a
+            className="group block overflow-hidden rounded-lg"
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={image || "desktop-design.jpg"}
+              alt={`An image of the ${name} website`}
+              className="transition-transform duration-300 group-hover:scale-110 "
+            />
+          </a>
+        </div>
+        <div className="row-start-4 row-end-6">
+          <a href={challengeLink}>{description}</a>
+        </div>
       </div>
     </article>
   );
